43625b108370ffe1b210773daed3efdd753371c5
ZERO <drio0037@gmail.com>
2016-04-01 14:29:50 +0300

    board2

---
+++ Models/StoneType.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GO.Models
+{
+    public enum StoneType { Black, White };
+}
---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: locbrag
+ * Date: 18.03.2016
+ * Time: 11:58
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using GO.Views;
+
+namespace GO
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+            var board = new GameWindow();
+		    board.ShowDialog();
+		}
+	}
+}
